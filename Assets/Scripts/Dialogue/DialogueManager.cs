a71a0c6a91e1a6c6fa56dba7e0121967779e38c6
randyRivera0 <152322555+randyRivera0@users.noreply.github.com>
2026-02-12 04:18:51 -0800

    Bug estacion 1 fixed

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -221,7 +221,7 @@ public class DialogueManager : MonoBehaviour
         if (currentEventToTrigger != "")
         {
             Debug.Log(currentEventToTrigger);
-            //EventManager.eventManager.TriggerEvent(currentEventToTrigger);
+            EventManager.eventManager.TriggerEvent(currentEventToTrigger);
         }
         EventManager.StopListening(currentEventToTrigger);
         if (removeDT)
@@ -284,8 +284,7 @@ public class DialogueManager : MonoBehaviour
                     dialogue.title[i] = traducido;
                 }
                 else
-                    Debug.LogWarning($"No se encontro traducci�n para la clave de t�tulo: {clave}");
-            }
+                    Debug.LogWarning($"No se encontro traducción para la clave de título: {clave}");            }
         }
 
         // Traducir sentences
@@ -300,8 +299,7 @@ public class DialogueManager : MonoBehaviour
                 if (!string.IsNullOrEmpty(traducido))
                     dialogue.sentences[i] = traducido;
                 else
-                    Debug.LogWarning($"No se encontro traducci�n para la clave de frase: {clave}");
-            }
+                    Debug.LogWarning($"No se encontro traducción para la clave de frase: {clave}");            }
         }
     }
 
